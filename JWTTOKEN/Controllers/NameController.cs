367f99ee7c20e701f038c3152e0aa0bd66f75150
Zeynep HASRET <zeynepphasrett@gmail.com>
2020-10-23 10:28:12 +0300

    first committ

---
+++ JWTTOKEN/Controllers/NameController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JWTTOKEN.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWTTOKEN.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager jwtAuthenticationManager;
+
+        public NameController(IJwtAuthenticationManager jwtAuthenticationManager)
+        {
+            this.jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+
+        // GET: api/<NameController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "New York", "New Jersey" };
+        }
+
+
+        // GET api/<NameController>/5
+        [HttpGet("{id}" ,Name ="Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = jwtAuthenticationManager.Authenticate(userCred.Username, userCred.Password);
+            if (token == null)
+            
+                return Unauthorized();
+              
+            return Ok(token);
+        }
+       
+    }
+}
